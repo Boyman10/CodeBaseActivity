8ac3c02721d809bdc8cbc439f73af46b7072b827
Eddy Mio <contact@remotehireman.fr>
2018-04-30 18:48:26 +0200

    Change in web.xml headers to allow EL

--- palabres/palabres-webapp/src/org/palabres/webapp/action/ManageUserAction.java
+++ palabres/palabres-webapp/src/org/palabres/webapp/action/ManageUserAction.java
@@ -1,9 +1,19 @@
 package org.palabres.webapp.action;
 
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ManageUserAction extends ActionSupport {
+public class ManageUserAction extends ActionSupport implements SessionAware {
 
 	private static final long serialVersionUID = -7149621975724165813L;
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
