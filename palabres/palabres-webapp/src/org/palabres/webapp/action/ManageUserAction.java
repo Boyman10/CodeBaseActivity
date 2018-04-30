00e89c08836dea01ff5bae0ad87139706991511a
Eddy Mio <contact@remotehireman.fr>
2018-04-30 19:20:38 +0200

    Adding dependencies to other level in webapp module pom

--- palabres/palabres-webapp/src/org/palabres/webapp/action/ManageUserAction.java
+++ palabres/palabres-webapp/src/org/palabres/webapp/action/ManageUserAction.java
@@ -10,13 +10,33 @@ public class ManageUserAction extends ActionSupport implements SessionAware {
 
 	private static final long serialVersionUID = -7149621975724165813L;
 
+	// Constant to be used to identify session
+	private static final String USER = "USER";
+
 	// Handling session
 	private Map<String, Object> userSession ;
+	
+	// The bean to be defined for the login form - using corresponding entity
+	private Utilisateur	userBean;
 
 	@Override
 	public void setSession(Map<String, Object> session) {
 
 		userSession = session;
 	}
-
+	
+	
+	/**
+	 * Login method checking user sign in
+	 * @return
+	 */
+	public String doLogin() {
+		
+		// Call service class to retrieve and compare state in database
+		
+		// Add user to session
+		userSession.put(USER, this.userBean);
+		
+		return ActionSupport.SUCCESS;
+}
 }
