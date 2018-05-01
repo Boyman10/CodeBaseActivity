4170242b09fae935bb173a58dbb9818a8ef74a75
Eddy Mio <contact@remotehireman.fr>
2018-05-01 08:45:42 +0200

    Linking Action class and doLogin doLogout to model/business module

--- palabres/palabres-webapp/src/org/palabres/webapp/action/ManageUserAction.java
+++ palabres/palabres-webapp/src/org/palabres/webapp/action/ManageUserAction.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 import org.example.palabres.model.bean.utilisateur.Utilisateur;
+import org.example.palabres.model.exception.NotFoundException;
+import org.palabres.webapp.helper.WebAppHelper;
+import org.apache.commons.lang3.StringUtils;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -15,12 +18,12 @@ public class ManageUserAction extends ActionSupport implements SessionAware {
 	private static final String USER = "USER";
 
 	// Handling session
-	private Map<String, Object> userSession ;
-	
+	private Map<String, Object> userSession;
+
 	// The bean to be defined for the login form - using corresponding entity
-	private Utilisateur	userBean;
+	private Utilisateur userBean;
 	private String password = "1234";
-	
+
 	/**
 	 * @return the password
 	 */
@@ -28,14 +31,13 @@ public class ManageUserAction extends ActionSupport implements SessionAware {
 		return password;
 	}
 
-
 	/**
-	 * @param password the password to set
+	 * @param password
+	 *            the password to set
 	 */
 	private void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	/**
 	 * @return the userBean
@@ -44,36 +46,54 @@ public class ManageUserAction extends ActionSupport implements SessionAware {
 		return userBean;
 	}
 
-
 	/**
-	 * @param userBean the userBean to set
+	 * @param userBean
+	 *            the userBean to set
 	 */
 	public void setUserBean(Utilisateur userBean) {
 		this.userBean = userBean;
 	}
-
 
 	@Override
 	public void setSession(Map<String, Object> session) {
 
 		userSession = session;
 	}
-	
-	
+
 	/**
 	 * Login method checking user sign in
+	 * 
 	 * @return
 	 */
 	public String doLogin() {
-		
-		// Par défaut, le result est "input"
+
         String vResult = ActionSupport.INPUT;
-        
-		// Call service class to retrieve and compare state in database
+        if (!StringUtils.isAllEmpty(userBean.getPseudo(), password)) {
+            try {
+                Utilisateur vUtilisateur
+                        = WebAppHelper.getManagerFactory().getUtilisateurManager()
+                                      .getUtilisateur(userBean.getPseudo());
+                
+                // Ajout de l'utilisateur en session
+                this.userSession.put(USER, vUtilisateur);
+                
+                vResult = ActionSupport.SUCCESS;
+            } catch (NotFoundException pEx) {
+                this.addActionError("Identifiant ou mot de passe invalide !");
+            }
+        }
+        return vResult;
+	}
+
+	/**
+	 * Action de déconnexion d'un utilisateur
+	 * 
+	 * @return success
+	 */
+	public String doLogout() {
 		
-		// Add user to session
-		userSession.put(USER, this.userBean);
-		
+	    this.userSession.remove(USER);
+
 		return ActionSupport.SUCCESS;
-}
+	}
 }
