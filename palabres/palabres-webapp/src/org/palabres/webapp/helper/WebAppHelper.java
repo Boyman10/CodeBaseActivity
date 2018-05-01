5a6a3a998b5a8e13130453eebe1f407b6e403eb4
Eddy Mio <contact@remotehireman.fr>
2018-05-01 08:55:04 +0200

    Adding webAppHelper to connect to our model/business faactory

---
+++ palabres/palabres-webapp/src/org/palabres/webapp/helper/WebAppHelper.java
@@ -0,0 +1,18 @@
+package org.palabres.webapp.helper;
+
+import org.example.palabres.business.contract.ManagerFactory;
+
+/**
+ * Classe de helper temporaire pour la webapp...
+ */
+public abstract class WebAppHelper {
+
+    private static ManagerFactory managerFactory;
+
+    public static ManagerFactory getManagerFactory() {
+        return managerFactory;
+    }
+    public static void setManagerFactory(ManagerFactory pManagerFactory) {
+        managerFactory = pManagerFactory;
+    }
+}
